11fb5d3a13c97cb9b3c75fd4d0bfc14b59ba03a6
IraEva <86668079+IraEva@users.noreply.github.com>
2021-07-10 17:34:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box=createsprite(200,200,10,10)
+
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+}
+
+if(keyIsDown(DOWN_ARROW))
+{
+  background ('blue')
+}
+
+if(keyIsDown(RIGHT_ARROW))
+{
+  background ('green')
+}
+
+if (keyIsDown(LEFT_ARROW))
+{
+  background('red')
+}
+
+if(keyIsDown(UP_ARROW))
+{
+  background('yellow')
+}
